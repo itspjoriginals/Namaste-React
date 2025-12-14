a9f1e47ff8b57724dc36b0c825f9ad0df2d0d0b5
Prashant Kumar Jha <itspjprashant07@gmail.com>
2025-12-15 03:01:50 +0530

    add foundational web dev intro with HTML/JS + React setup

---
+++ Day1- Inception/App.js
@@ -0,0 +1,17 @@
+
+const parent = React.createElement('div', { "id": "div1" }, [React.createElement('div', { "id": "div2" }, [React.createElement('h1', { "id": "heading1" }, "This is Tag1" ),
+    React.createElement('h2', { "id": "heading2" }, "This is Tag2" )
+    
+] ),
+
+React.createElement('div', { "id": "div2" }, [React.createElement('h1', { "id": "heading1" }, "This is Tag1" ),
+    React.createElement('h2', { "id": "heading2" }, "This is Tag2" )
+    
+] )]);
+
+
+// const heading = React.createElement('h1', {"id":"heading"}, "This is Tag1");
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent);
