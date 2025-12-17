603ee0fb1c96727c68cc457efe6eb55bd279d1ea
Prashant Kumar Jha <itspjprashant07@gmail.com>
2025-12-17 13:01:08 +0530

    Laying the Foundation (JSX, components, React.createElement)

--- Day3-Laying-the-foundation/App.js
+++ Day3-Laying-the-foundation/App.js
@@ -3,7 +3,18 @@ import ReactDOM from "react-dom/client";
 
 const jsxHeading = (<h1 className="heading">Namaste React using JSX</h1>)
 
+const Title = ()=> {
+    return <h1>This is Title React Component </h1>
+}
+
+const HeaderComponent = ()=>{
+    return <div>
+        <Title />
+        <h1>Namaste React Header Component</h1>
+    </div>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
-root.render(jsxHeading);
+root.render(<HeaderComponent />);
