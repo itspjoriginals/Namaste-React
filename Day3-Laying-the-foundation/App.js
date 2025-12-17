40b224a293f85c47f6d7aa99b0bde9da69327471
Prashant Kumar Jha <itspjprashant07@gmail.com>
2025-12-17 12:21:52 +0530

    Add Day3 of namaste-react

---
+++ Day3-Laying-the-foundation/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const jsxHeading = (<h1 className="heading">Namaste React using JSX</h1>)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(jsxHeading);
